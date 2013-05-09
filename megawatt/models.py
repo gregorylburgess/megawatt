67d7765d5941ce79c123de2d06537b8f41869be0
gregorylburgess <gregory.l.burgess@gmail.com>
2013-05-08 22:50:48 -1000

    Reverted UI change.

--- megawatt/models.py
+++ megawatt/models.py
@@ -13,8 +13,10 @@ class PointSet(models.Model):
 class Card(models.Model):
     name = models.CharField(max_length=200)
     type = ("E_Gen", "Tactical", "Service")
-    flavor_text = models.CharField(max_length=100)
+    flavor_text = models.CharField(max_length=300)
     success_val = models.IntegerField()
+    success_text = models.CharField(max_length=100)
+    fail_text = models.CharField(max_length=100)
     image = models.CharField(max_length=200)
     owner = models.ForeignKey(Player)
     def __unicode__(self):
@@ -22,9 +24,18 @@ class Card(models.Model):
 
 
 class GameState(models.Model):
+    p1 = models.ForeignKey(Player)
+    p2 = models.ForeignKey(Player)
+    p1_hand={}
+    p2_hand={}
+    p1_field={}
+    p2_field={}
     player_cards = {}
-    player_scores = {}
     neutral_cards= {}
+    tactical_deck=[]
+    e_gen_deck=[]
+    p1_score = models.ForeignKey(PointSet)
+    p2_score = models.ForeignKey(PointSet)
     active_player = ("p1","p2")
-    phase = ("U","B","R","G","E")
+    phase = ("U","B","R","I","G","R")
     
